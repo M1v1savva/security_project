0caf2b5702ab48100d5ea2688cac1027bb8deab6
M1v1savva <ivan.polyakov.01@gmail.com>
2021-09-17 01:14:46 +0200

    basic connection, now its time for security

--- client.py
+++ client.py
@@ -1,8 +1,37 @@
-import sys
-import json
 import requests
+import json
+import time
 
-conv = [{'input': 'hi', 'topic': 'Greeting'}]
-s = json.dumps(conv)
-res = requests.post("http://127.0.0.1:5000/determine_escalation/", json=s).json()
-print(res['escalate'])
+with open('config_1.json') as config_file:
+    config = json.load(config_file)
+server_url = "http://" + config["server"]["ip"] + ":" + config["server"]["port"]
+
+message = {"id": config["id"], "password": config["password"]}
+res = requests.post(server_url + "/register/", json=json.dumps(message)).json()
+#print(dt)
+#print(res)
+
+#if res != 0:
+#    print('error ' + str(res))
+#    exit(0)
+#print(res)
+
+url_nxt = res["url"]
+actions = config["actions"]["steps"]
+delay = int(config["actions"]["delay"])
+
+for item in actions:
+    action, value = item.split(' ')
+    value = int(value)
+    if action == 'DECREASE':
+        value *= -1
+    message = {'id': config["id"], 'delta': value}
+    res = requests.post(server_url + url_nxt, json=json.dumps(message)).json()
+
+#     if res != 0:
+#         print('error ' + str(res))
+#         exit(0)
+    time.sleep(delay)
+
+message = {'id': config["id"]}
+res = requests.post(server_url + '/close/', json=json.dumps(message)).json()
--- server.py
+++ server.py
@@ -1,19 +1,59 @@
-from flask import Flask
-from flask import request
+from flask import Flask, request, url_for
 import json
 
 app = Flask(__name__)
 
-@app.route('/determine_escalation/', methods = ['POST'])
-def determine_escalation():
+id_present = dict()
+id_password = dict()
+id_url = dict()
+id_value = dict()
+
+@app.route('/register/', methods = ['POST'])
+def register():
     jsondata = request.get_json()
     data = json.loads(jsondata)
+    id = data["id"]
+    password = data["password"]
 
-    #stuff happens here that involves data to obtain a result
+    if id in id_password and id_password[id] != password:
+        return {}, 401
 
-    result = {'escalate': True}
+    if id not in id_present:
+        id_present[id] = 1
+        id_url[id] = url_for("update")
+    else:
+        id_present[id] += 1
+    id_password[id] = password
+    id_value[id] = 0
+
+    result = {"url": id_url[id]}
     return json.dumps(result)
 
+@app.route('/update/', methods = ['POST'])
+def update():
+    jsondata = request.get_json()
+    data = json.loads(jsondata)
+    id = data["id"]
+    val = data["delta"]
+    id_value[id] += val
+    print(id_value[id])
+    result = {"response": 0}
+    return json.dumps(result)
+
+@app.route('/close/', methods = ['POST'])
+def close():
+    jsondata = request.get_json()
+    data = json.loads(jsondata)
+    id = data["id"]
+    id_present[id] -= 1
+    if id_present[id] == 0:
+        id_present.pop(id, None)
+        id_password.pop(id, None)
+        id_url.pop(id, None)
+        id_value.pop(id, None)
+
+    result = {"response": 0}
+    return json.dumps(result)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=13370)
